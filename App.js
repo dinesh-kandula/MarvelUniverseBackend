02f4c6d3b3fcfee46c4547ef1916c8a4f420e091
dinesh-kandula <dinesh.kandula@techvedika.com>
2023-09-13 12:53:35 +0530

    Added login and crew API's

--- App.js
+++ App.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello, Dinnu Bunny!');
 });
 
+app.get('/login', (req, res) => {
+  res.send('Hello, It is Login Page');
+});
+
+app.get('/crew', (req, res) => {
+  res.send('Hello, It is Crew Data Page');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port} Hello Dinnu Bunny`);
 });
